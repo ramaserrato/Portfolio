40aa23a61573aef499446ba4b5eb9db0372dfbc5
Ramiro Serrato <ramaserrato@gmail.com>
2026-07-28 16:42:18 -0300

    cambios en vite config

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import es from "./locales/Es.json";
-import en from "./locales/En.json";
+
+const es = await import("./locales/Es.json", { assert: { type: "json" } });
+const en = await import("./locales/En.json", { assert: { type: "json" } });
 
 i18n
   .use(initReactI18next)
