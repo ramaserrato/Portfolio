290279504e3dcdd04d9e8ffcb9ab0d3c0ca14392
Ramiro Serrato <ramaserrato@gmail.com>
2026-07-28 17:02:03 -0300

    a ver si funciona

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import es from "./locales/es.json";
-import en from "./locales/en.json";
+import es from "./locales/es";
+import en from "./locales/en";
 
 i18n
   .use(initReactI18next)
