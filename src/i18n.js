a137158210ce3c0030bbd3fa718138691d0fa27f
Ramiro Serrato <ramaserrato@gmail.com>
2026-07-28 16:35:46 -0300

    cambio de nombres de archivos

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import es from "./locales/es.json";
-import en from "./locales/en.json";
+import es from "./locales/Es.json";
+import en from "./locales/En.json";
 
 i18n
   .use(initReactI18next)
